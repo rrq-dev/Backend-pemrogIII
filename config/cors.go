ffea4ad7fa43d3d0657de9306a8edf5160fdec47
rrq-dev <raihanhermawan85@gmail.com>
2025-07-07 14:18:50 +0700

    CODE: Add additional allowed origin for CORS configuration

--- config/cors.go
+++ config/cors.go
@@ -4,6 +4,7 @@ var AllowedOrigins = []string{
 	"http://localhost:3000",
 	"http://localhost:5173",
 	"http://localhost:6969",
+	"https://ubiquitous-belekoy-512d48.netlify.app/",
 }
 
 func GetAllowedOrigins() []string {
