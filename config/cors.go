20ccc325cc988bab73f91cc1e40c415dd02de34b
rrq-dev <raihanhermawan85@gmail.com>
2025-07-07 15:07:24 +0700

    Update allowed origins in CORS configuration

--- config/cors.go
+++ config/cors.go
@@ -4,7 +4,7 @@ var AllowedOrigins = []string{
 	"http://localhost:3000",
 	"http://localhost:5173",
 	"http://localhost:6969",
-	"https://ubiquitous-belekoy-512d48.netlify.app/",
+	"https://mahasiswapage.netlify.app/",
 }
 
 func GetAllowedOrigins() []string {
